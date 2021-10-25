99d89fdef7ee9ee236568e0e9bcb804a1de7a16e
viaroska <92136376+viaroska@users.noreply.github.com>
2021-10-25 15:27:14 -0300

    1.4

--- app/src/main/java/cl/ipvg/certamen1viaroskazamorano/MainActivityUno.java
+++ app/src/main/java/cl/ipvg/certamen1viaroskazamorano/MainActivityUno.java
@@ -26,10 +26,9 @@ public class MainActivityUno extends AppCompatActivity {
         Nombre= (TextView) findViewById(R.id.NaAlumno);
         Nombre.setText(Alumno1);
         Informacion2= (TextView) findViewById(R.id.Informacion1);
-        Informacion2.setText("El o la Estudiante"+Alumno1 +"   y su promedio es:");
+        Informacion2.setText("El o la Estudiante"+Alumno1 + "   y Su Promedio Es:");
 
         BtnR.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 Intent BtnR = new Intent(MainActivityUno.this,MainActivity.class);
