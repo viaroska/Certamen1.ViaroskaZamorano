ce76769443d65e369891e67c22d57358f220c6ac
viaroska <92136376+viaroska@users.noreply.github.com>
2021-10-25 14:27:02 -0300

    1.1

--- app/src/main/java/cl/ipvg/certamen1viaroskazamorano/MainActivity.java
+++ app/src/main/java/cl/ipvg/certamen1viaroskazamorano/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Edittext Nombre;
+    EditText Nombre;
     EditText Carrera;
     Button BtnCalcular;
 
@@ -15,7 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Carrera = (EditText)findViewById(R.id.CaCarrera);
         Intent act = new Intent(this,MainActivityUno.class);
+        Nombre= (EditText)findViewById (R.id.NaNombre);
+        BtnCalcular = (Button)findViewById(R.id.BtnCalcular1);
+        BtnCalcular.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                act.putExtra("Nombre",Nombre.getText().toString());
+
+                Intent BtnCalcular = new Intent(MainActivity.this,MainActivityUno.class);
+
+                startActivity(act);
+            }
+        });
     }
 }
--- app/src/main/java/cl/ipvg/certamen1viaroskazamorano/MainActivityUno.java
+++ app/src/main/java/cl/ipvg/certamen1viaroskazamorano/MainActivityUno.java
@@ -2,13 +2,48 @@ package cl.ipvg.certamen1viaroskazamorano;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivityUno extends AppCompatActivity {
+    private TextView Nombre;
+    private Button BtnR;
+    private TextView Informacion2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_uno);
+        RecibirInfoAlumnos();
+        BtnR=(Button)findViewById(R.id.btnB);
+        Bundle extras = getIntent().getExtras();
+        String Alumno1 = extras.getString("Nombre");
+
+
+        Nombre= (TextView) findViewById(R.id.NaAlumno);
+        Nombre.setText(Alumno1);
+
+        Informacion2= (TextView) findViewById(R.id.Informacion1);
+        Informacion2.setText("El Estudiante"+Alumno1 +" y su promedio es:");
+
+        BtnR.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent BtnR = new Intent(MainActivityUno.this,MainActivity.class);
+
+                startActivity(BtnR);
+            }
+        });
+
+    }
+    public void  RecibirInfoAlumnos(){
+        Bundle Informacion2_ = getIntent().getExtras();
+
     }
 }
